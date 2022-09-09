b608bfe05218d21c06e3cfbf77abe720d29bd94a
AdnanBukvic <89768167+AdnanBukvic@users.noreply.github.com>
2022-09-09 14:33:29 +0200

    Add project files.

---
+++ Uppgift 2.10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_2_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Vilken är din favoritbok?");
+
+            string x = Console.ReadLine();
+         
+            Console.WriteLine("Din favoritbok är " + x + " ");
+            String resemål = Console.ReadLine();
+     
+        }
+    }
+}
+
